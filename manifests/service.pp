a60f4bf561688f4784d5fdcedd86a3470ed78c70
Adam Breaux <breauxa@email.chop.edu>
2015-04-17 16:14:53 -0400

    missing service wrapper

--- manifests/service.pp
+++ manifests/service.pp
@@ -13,7 +13,16 @@ define supervisor::service (
   service { $service:
     ensure  => $ensure,
     enable  => $enable,
+    require => File['/etc/init.d/supervisord'],
     require => Package[$required],
+  }
+
+  file { '/etc/init.d/supervisord':
+    ensure => present,
+    owner  => 'root',
+    group  => 'root',
+    mode   => '0755',
+    source => 'puppet:///modules/supervisor/supervisord.init'
   }
 
 }
