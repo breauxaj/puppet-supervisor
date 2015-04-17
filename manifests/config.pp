d4a6ff5b9ba8ef257557a745e1d0518a358e8358
Adam Breaux <breauxa@email.chop.edu>
2015-04-17 09:40:15 -0400

    overhaul

--- manifests/config.pp
+++ manifests/config.pp
@@ -1,21 +1,24 @@
 define supervisor::config (
-
+  $value
 ) {
+  $required = $::operatingsystem ? {
+    /(?i-mx:centos|fedora|redhat|scientific)/ => [ 'supervisor' ],
+  }
+
   $service = $::operatingsystem ? {
-    /(?i-mx:centos|fedora|redhat|scientific)/ => 'supervisord',
+    /(?i-mx:centos|fedora|redhat|scientific)/ => [ 'supervisord' ],
   }
 
-  $config = $::operatingsystem ? {
-    /(?i-mx:centos|fedora|redhat|scientific)/ => '/etc/supervisord.conf',
-  }
+  $key = $title
 
-  file { $config:
-    ensure  => present,
-    owner   => 'root',
-    group   => 'root',
-    mode    => '0644',
-    content => template('supervisord/supervisord.erb'),
+  $context = '/files/etc/supervisord.conf'
+
+  augeas { "supervisord_conf/${key}":
+    context => $context,
+    onlyif  => "get ${key} != '${value}'",
+    changes => "set ${key} '${value}'",
     notify  => Service[$service],
+    require => Package[$required],
   }
 
 }
