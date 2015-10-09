76312a86f7cb052919856397b308edb35ad5e6a6
Adam Breaux <breauxaj@gmail.com>
2015-10-09 00:48:16 -0400

    try a diff lens

--- manifests/config.pp
+++ manifests/config.pp
@@ -10,7 +10,7 @@ define supervisor::config (
   }
 
   augeas { "supervisord_conf/${key}":
-    lens    => 'IniFile.lns',
+    lens    => 'Yum.lns',
     incl    => $config,
     onlyif  => "get ${key} != '${value}'",
     changes => "set ${key} '${value}'",
