d708305adaabff5a0d0944b4f1be988727d41696
Adam Breaux <breauxaj@gmail.com>
2015-10-08 22:50:39 -0400

    see if it takes care of it?

--- manifests/init.pp
+++ manifests/init.pp
@@ -2,7 +2,7 @@ class supervisor (
   $ensure = 'latest',
 ) {
   $required = $::operatingsystem ? {
-    /(?i-mx:centos|fedora|redhat|scientific)/ => [ 'supervisor' ],
+    /(?i-mx:centos|fedora|redhat|scientific)/ => [ 'supervisor', 'distribute' ],
   }
 
   $config = $::operatingsystem ? {
