9c18e6121244cf2ceaac45952812aec6aa5406de
Adam Breaux <breauxaj@gmail.com>
2015-10-11 13:33:26 -0400

    support for rhel7

--- manifests/service.pp
+++ manifests/service.pp
@@ -12,21 +12,58 @@ define supervisor::service (
     /(?i-mx:centos|fedora|redhat|scientific)/ => 'supervisord',
   }
 
-  service { $service:
-    ensure  => $ensure,
-    enable  => $enable,
-    require => [
-      File['/etc/init.d/supervisord'],
-      Package[$required]
-    ]
-  }
+  case $::osfamily {
+    'redhat': {
+      case $::lsbmajdistrelease {
+        '6': {
+          service { $service:
+            ensure  => $ensure,
+            enable  => $enable,
+            require => [
+              File['/etc/init.d/supervisord'],
+              Package[$required]
+            ]
+          }
 
-  file { '/etc/init.d/supervisord':
-    ensure => present,
-    owner  => 'root',
-    group  => 'root',
-    mode   => '0755',
-    source => 'puppet:///modules/supervisor/supervisord.init'
+          file { '/etc/init.d/supervisord':
+            ensure => present,
+            owner  => 'root',
+            group  => 'root',
+            mode   => '0755',
+            source => 'puppet:///modules/supervisor/supervisord.init'
+          }
+        }
+        default: {
+          include ::systemctl
+
+          service { $service:
+            ensure  => $ensure,
+            enable  => $enable,
+            require => File['/lib/systemd/system/supervisord.service']
+          }
+
+          file { '/lib/systemd/system/supervisord.service':
+            ensure => present,
+            owner  => 'root',
+            group  => 'root',
+            mode   => '0755',
+            source => 'puppet:///modules/supervisor/supervisord.service'
+          }
+
+          file { '/etc/systemd/system/supervisord.service':
+            ensure  => 'link',
+            owner   => 'root',
+            group   => 'root',
+            target  => '/lib/systemd/system/supervisord.service'
+          }
+
+          systemctl::reload { 'supervisord': }
+        }
+      }
+    }
+    default: {
+      notify { 'OS not supported by this module': }
+    }
   }
 
 }
